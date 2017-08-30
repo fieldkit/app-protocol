7eb64e0ae84b7ceae28dc8143297cda7ec2c8832
Jacob Lewallen <jlewalle@gmail.com>
2017-08-30 11:16:48 -0700

    Starting on actual protocol.

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all: fk-app.proto.json fk-app.pb.go 
+all: fk-app.proto.json fk-app.pb.go fk-app.pb.c fk-app.pb.h
 
 node_modules/.bin/pbjs:
 	npm install
 
 fk-app.proto.json: node_modules/.bin/pbjs fk-app.proto
 	pbjs fk-app.proto -t json -o fk-app.proto.json
+
+fk-app.pb.c fk-app.pb.h: fk-app.proto
+	protoc --nanopb_out=./ fk-app.proto
 
 fk-app.pb.go: fk-app.proto
 	protoc --go_out=./ fk-app.proto
--- fk-app.pb.go
+++ fk-app.pb.go
@@ -8,10 +8,11 @@ It is generated from these files:
 	fk-app.proto
 
 It has these top-level messages:
-	PingRequest
-	PingResponse
-	RequestHeader
-	ResponseHeader
+	QueryCapabilities
+	SensorCapabilities
+	Capabilities
+	WireMessageQuery
+	WireMessageReply
 */
 package fk_app
 
@@ -30,125 +31,200 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-type RequestHeader_MessageType int32
+type QueryType int32
 
 const (
-	RequestHeader_PING      RequestHeader_MessageType = 0
-	RequestHeader_SAY_HELLO RequestHeader_MessageType = 1
+	QueryType_QUERY_NONE         QueryType = 0
+	QueryType_QUERY_CAPABILITIES QueryType = 1
 )
 
-var RequestHeader_MessageType_name = map[int32]string{
-	0: "PING",
-	1: "SAY_HELLO",
+var QueryType_name = map[int32]string{
+	0: "QUERY_NONE",
+	1: "QUERY_CAPABILITIES",
 }
-var RequestHeader_MessageType_value = map[string]int32{
-	"PING":      0,
-	"SAY_HELLO": 1,
-}
-
-func (x RequestHeader_MessageType) String() string {
-	return proto.EnumName(RequestHeader_MessageType_name, int32(x))
-}
-func (RequestHeader_MessageType) EnumDescriptor() ([]byte, []int) { return fileDescriptor0, []int{2, 0} }
-
-type PingRequest struct {
-	Time int64 `protobuf:"varint,1,opt,name=time" json:"time,omitempty"`
+var QueryType_value = map[string]int32{
+	"QUERY_NONE":         0,
+	"QUERY_CAPABILITIES": 1,
 }
 
-func (m *PingRequest) Reset()                    { *m = PingRequest{} }
-func (m *PingRequest) String() string            { return proto.CompactTextString(m) }
-func (*PingRequest) ProtoMessage()               {}
-func (*PingRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+func (x QueryType) String() string {
+	return proto.EnumName(QueryType_name, int32(x))
+}
+func (QueryType) EnumDescriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
-func (m *PingRequest) GetTime() int64 {
+type ReplyType int32
+
+const (
+	ReplyType_REPLY_NONE         ReplyType = 0
+	ReplyType_REPLY_CAPABILITIES ReplyType = 1
+)
+
+var ReplyType_name = map[int32]string{
+	0: "REPLY_NONE",
+	1: "REPLY_CAPABILITIES",
+}
+var ReplyType_value = map[string]int32{
+	"REPLY_NONE":         0,
+	"REPLY_CAPABILITIES": 1,
+}
+
+func (x ReplyType) String() string {
+	return proto.EnumName(ReplyType_name, int32(x))
+}
+func (ReplyType) EnumDescriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+
+type QueryCapabilities struct {
+	Version int64 `protobuf:"varint,1,opt,name=version" json:"version,omitempty"`
+}
+
+func (m *QueryCapabilities) Reset()                    { *m = QueryCapabilities{} }
+func (m *QueryCapabilities) String() string            { return proto.CompactTextString(m) }
+func (*QueryCapabilities) ProtoMessage()               {}
+func (*QueryCapabilities) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+
+func (m *QueryCapabilities) GetVersion() int64 {
 	if m != nil {
-		return m.Time
+		return m.Version
 	}
 	return 0
 }
 
-type PingResponse struct {
-	Time int64 `protobuf:"varint,1,opt,name=time" json:"time,omitempty"`
+type SensorCapabilities struct {
+	Index int32  `protobuf:"varint,1,opt,name=index" json:"index,omitempty"`
+	Name  string `protobuf:"bytes,2,opt,name=name" json:"name,omitempty"`
 }
 
-func (m *PingResponse) Reset()                    { *m = PingResponse{} }
-func (m *PingResponse) String() string            { return proto.CompactTextString(m) }
-func (*PingResponse) ProtoMessage()               {}
-func (*PingResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (m *SensorCapabilities) Reset()                    { *m = SensorCapabilities{} }
+func (m *SensorCapabilities) String() string            { return proto.CompactTextString(m) }
+func (*SensorCapabilities) ProtoMessage()               {}
+func (*SensorCapabilities) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
-func (m *PingResponse) GetTime() int64 {
+func (m *SensorCapabilities) GetIndex() int32 {
 	if m != nil {
-		return m.Time
+		return m.Index
 	}
 	return 0
 }
 
-type RequestHeader struct {
-	Type RequestHeader_MessageType `protobuf:"varint,1,opt,name=type,enum=fk_app.RequestHeader_MessageType" json:"type,omitempty"`
-}
-
-func (m *RequestHeader) Reset()                    { *m = RequestHeader{} }
-func (m *RequestHeader) String() string            { return proto.CompactTextString(m) }
-func (*RequestHeader) ProtoMessage()               {}
-func (*RequestHeader) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
-
-func (m *RequestHeader) GetType() RequestHeader_MessageType {
+func (m *SensorCapabilities) GetName() string {
 	if m != nil {
-		return m.Type
-	}
-	return RequestHeader_PING
-}
-
-type ResponseHeader struct {
-	Success bool   `protobuf:"varint,1,opt,name=success" json:"success,omitempty"`
-	Error   string `protobuf:"bytes,2,opt,name=error" json:"error,omitempty"`
-}
-
-func (m *ResponseHeader) Reset()                    { *m = ResponseHeader{} }
-func (m *ResponseHeader) String() string            { return proto.CompactTextString(m) }
-func (*ResponseHeader) ProtoMessage()               {}
-func (*ResponseHeader) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
-
-func (m *ResponseHeader) GetSuccess() bool {
-	if m != nil {
-		return m.Success
-	}
-	return false
-}
-
-func (m *ResponseHeader) GetError() string {
-	if m != nil {
-		return m.Error
+		return m.Name
 	}
 	return ""
 }
 
+type Capabilities struct {
+	Version int32                 `protobuf:"varint,1,opt,name=version" json:"version,omitempty"`
+	Name    string                `protobuf:"bytes,3,opt,name=name" json:"name,omitempty"`
+	Sensors []*SensorCapabilities `protobuf:"bytes,4,rep,name=sensors" json:"sensors,omitempty"`
+}
+
+func (m *Capabilities) Reset()                    { *m = Capabilities{} }
+func (m *Capabilities) String() string            { return proto.CompactTextString(m) }
+func (*Capabilities) ProtoMessage()               {}
+func (*Capabilities) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
+
+func (m *Capabilities) GetVersion() int32 {
+	if m != nil {
+		return m.Version
+	}
+	return 0
+}
+
+func (m *Capabilities) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+func (m *Capabilities) GetSensors() []*SensorCapabilities {
+	if m != nil {
+		return m.Sensors
+	}
+	return nil
+}
+
+type WireMessageQuery struct {
+	Type              QueryType          `protobuf:"varint,1,opt,name=type,enum=fk_app.QueryType" json:"type,omitempty"`
+	QueryCapabilities *QueryCapabilities `protobuf:"bytes,2,opt,name=queryCapabilities" json:"queryCapabilities,omitempty"`
+}
+
+func (m *WireMessageQuery) Reset()                    { *m = WireMessageQuery{} }
+func (m *WireMessageQuery) String() string            { return proto.CompactTextString(m) }
+func (*WireMessageQuery) ProtoMessage()               {}
+func (*WireMessageQuery) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
+
+func (m *WireMessageQuery) GetType() QueryType {
+	if m != nil {
+		return m.Type
+	}
+	return QueryType_QUERY_NONE
+}
+
+func (m *WireMessageQuery) GetQueryCapabilities() *QueryCapabilities {
+	if m != nil {
+		return m.QueryCapabilities
+	}
+	return nil
+}
+
+type WireMessageReply struct {
+	Type         ReplyType     `protobuf:"varint,1,opt,name=type,enum=fk_app.ReplyType" json:"type,omitempty"`
+	Capabilities *Capabilities `protobuf:"bytes,2,opt,name=capabilities" json:"capabilities,omitempty"`
+}
+
+func (m *WireMessageReply) Reset()                    { *m = WireMessageReply{} }
+func (m *WireMessageReply) String() string            { return proto.CompactTextString(m) }
+func (*WireMessageReply) ProtoMessage()               {}
+func (*WireMessageReply) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
+
+func (m *WireMessageReply) GetType() ReplyType {
+	if m != nil {
+		return m.Type
+	}
+	return ReplyType_REPLY_NONE
+}
+
+func (m *WireMessageReply) GetCapabilities() *Capabilities {
+	if m != nil {
+		return m.Capabilities
+	}
+	return nil
+}
+
 func init() {
-	proto.RegisterType((*PingRequest)(nil), "fk_app.PingRequest")
-	proto.RegisterType((*PingResponse)(nil), "fk_app.PingResponse")
-	proto.RegisterType((*RequestHeader)(nil), "fk_app.RequestHeader")
-	proto.RegisterType((*ResponseHeader)(nil), "fk_app.ResponseHeader")
-	proto.RegisterEnum("fk_app.RequestHeader_MessageType", RequestHeader_MessageType_name, RequestHeader_MessageType_value)
+	proto.RegisterType((*QueryCapabilities)(nil), "fk_app.QueryCapabilities")
+	proto.RegisterType((*SensorCapabilities)(nil), "fk_app.SensorCapabilities")
+	proto.RegisterType((*Capabilities)(nil), "fk_app.Capabilities")
+	proto.RegisterType((*WireMessageQuery)(nil), "fk_app.WireMessageQuery")
+	proto.RegisterType((*WireMessageReply)(nil), "fk_app.WireMessageReply")
+	proto.RegisterEnum("fk_app.QueryType", QueryType_name, QueryType_value)
+	proto.RegisterEnum("fk_app.ReplyType", ReplyType_name, ReplyType_value)
 }
 
 func init() { proto.RegisterFile("fk-app.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 247 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x49, 0xcb, 0xd6, 0x4d,
-	0x2c, 0x28, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x4b, 0xcb, 0x8e, 0x4f, 0x2c, 0x28,
-	0x50, 0x52, 0xe4, 0xe2, 0x0e, 0xc8, 0xcc, 0x4b, 0x0f, 0x4a, 0x2d, 0x2c, 0x4d, 0x2d, 0x2e, 0x11,
-	0x12, 0xe2, 0x62, 0x29, 0xc9, 0xcc, 0x4d, 0x95, 0x60, 0x54, 0x60, 0xd4, 0x60, 0x0e, 0x02, 0xb3,
-	0x95, 0x94, 0xb8, 0x78, 0x20, 0x4a, 0x8a, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0xb1, 0xaa, 0xc9, 0xe3,
-	0xe2, 0x85, 0x1a, 0xe1, 0x91, 0x9a, 0x98, 0x92, 0x5a, 0x24, 0x64, 0xca, 0xc5, 0x52, 0x52, 0x59,
-	0x00, 0x51, 0xc4, 0x67, 0xa4, 0xa8, 0x07, 0xb1, 0x4e, 0x0f, 0x45, 0x91, 0x9e, 0x6f, 0x6a, 0x71,
-	0x71, 0x62, 0x7a, 0x6a, 0x48, 0x65, 0x41, 0x6a, 0x10, 0x58, 0xb9, 0x92, 0x1a, 0x17, 0x37, 0x92,
-	0xa0, 0x10, 0x07, 0x17, 0x4b, 0x80, 0xa7, 0x9f, 0xbb, 0x00, 0x83, 0x10, 0x2f, 0x17, 0x67, 0xb0,
-	0x63, 0x64, 0xbc, 0x87, 0xab, 0x8f, 0x8f, 0xbf, 0x00, 0xa3, 0x92, 0x03, 0x17, 0x1f, 0xcc, 0x3d,
-	0x50, 0x0b, 0x25, 0xb8, 0xd8, 0x8b, 0x4b, 0x93, 0x93, 0x53, 0x8b, 0x8b, 0xc1, 0x76, 0x72, 0x04,
-	0xc1, 0xb8, 0x42, 0x22, 0x5c, 0xac, 0xa9, 0x45, 0x45, 0xf9, 0x45, 0x12, 0x4c, 0x0a, 0x8c, 0x1a,
-	0x9c, 0x41, 0x10, 0x8e, 0x91, 0x1d, 0x17, 0x7b, 0x70, 0x6a, 0x51, 0x59, 0x66, 0x72, 0xaa, 0x90,
-	0x31, 0x17, 0x0b, 0xc8, 0x83, 0x42, 0xc2, 0x30, 0x57, 0x22, 0x85, 0x88, 0x94, 0x08, 0xaa, 0x20,
-	0xc4, 0x4e, 0x25, 0x86, 0x24, 0x36, 0x70, 0x38, 0x1a, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x42,
-	0x4c, 0x1a, 0x5c, 0x57, 0x01, 0x00, 0x00,
+	// 317 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x92, 0xcf, 0x4b, 0xfb, 0x40,
+	0x14, 0xc4, 0xbf, 0xf9, 0xf6, 0x17, 0x7d, 0x2d, 0xa5, 0x5d, 0x8a, 0x44, 0x4f, 0x25, 0x20, 0x14,
+	0xa1, 0x39, 0xa4, 0x1e, 0x3c, 0x09, 0xb5, 0x04, 0x09, 0xd4, 0xda, 0x6e, 0x2b, 0xe2, 0xa9, 0x6c,
+	0xf5, 0x55, 0x96, 0xd6, 0x64, 0xdd, 0x8d, 0x62, 0xae, 0xfe, 0xe5, 0xd2, 0x0d, 0x89, 0x5d, 0x83,
+	0xde, 0x32, 0xbc, 0xf9, 0x64, 0x66, 0x42, 0xa0, 0xb9, 0xd9, 0x0e, 0x98, 0x10, 0xae, 0x90, 0x51,
+	0x1c, 0x91, 0xea, 0x66, 0xbb, 0x62, 0x42, 0x38, 0x03, 0xe8, 0xcc, 0xdf, 0x50, 0x26, 0x63, 0x26,
+	0xd8, 0x9a, 0xef, 0x78, 0xcc, 0x51, 0x11, 0x1b, 0x6a, 0xef, 0x28, 0x15, 0x8f, 0x42, 0xdb, 0xea,
+	0x59, 0xfd, 0x12, 0xcd, 0xa4, 0x73, 0x09, 0x64, 0x81, 0xa1, 0x8a, 0xa4, 0xe1, 0xef, 0x42, 0x85,
+	0x87, 0x4f, 0xf8, 0xa1, 0xdd, 0x15, 0x9a, 0x0a, 0x42, 0xa0, 0x1c, 0xb2, 0x17, 0xb4, 0xff, 0xf7,
+	0xac, 0x7e, 0x9d, 0xea, 0x67, 0x47, 0x42, 0xf3, 0xaf, 0xa4, 0x4a, 0x9e, 0x94, 0xd3, 0xa5, 0x6f,
+	0x9a, 0x9c, 0x43, 0x4d, 0xe9, 0x74, 0x65, 0x97, 0x7b, 0xa5, 0x7e, 0xc3, 0x3b, 0x71, 0xd3, 0x19,
+	0x6e, 0xb1, 0x14, 0xcd, 0xac, 0xce, 0xa7, 0x05, 0xed, 0x7b, 0x2e, 0xf1, 0x06, 0x95, 0x62, 0xcf,
+	0xa8, 0xe7, 0x92, 0x53, 0x28, 0xc7, 0x89, 0x40, 0x9d, 0xda, 0xf2, 0x3a, 0xd9, 0x7b, 0xf4, 0x71,
+	0x99, 0x08, 0xa4, 0xfa, 0x4c, 0xae, 0xa1, 0xf3, 0xfa, 0xf3, 0xf3, 0xe8, 0x41, 0x0d, 0xef, 0xd8,
+	0x60, 0x8c, 0xe8, 0x22, 0xe3, 0x28, 0xa3, 0x03, 0x45, 0xb1, 0xfb, 0xb5, 0x83, 0x3e, 0x1e, 0x74,
+	0xb8, 0x80, 0xe6, 0x63, 0x31, 0xbe, 0x9b, 0xd9, 0x8d, 0x64, 0xc3, 0x79, 0x36, 0x84, 0x7a, 0x3e,
+	0x88, 0xb4, 0x00, 0xe6, 0x77, 0x3e, 0x7d, 0x58, 0x4d, 0x6f, 0xa7, 0x7e, 0xfb, 0x1f, 0x39, 0x02,
+	0x92, 0xea, 0xf1, 0x68, 0x36, 0xba, 0x0a, 0x26, 0xc1, 0x32, 0xf0, 0x17, 0x6d, 0x6b, 0x0f, 0xe5,
+	0x0d, 0xf6, 0x10, 0xf5, 0x67, 0x93, 0x43, 0x28, 0xd5, 0x26, 0xb4, 0xae, 0xea, 0xbf, 0x6a, 0xf8,
+	0x15, 0x00, 0x00, 0xff, 0xff, 0xe2, 0x9d, 0x73, 0x70, 0x65, 0x02, 0x00, 0x00,
 }
