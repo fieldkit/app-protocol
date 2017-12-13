92784c689ef6fd6e8162d33db09250ba0af01660
Jacob Lewallen <jlewallen@gmail.com>
2017-12-13 12:07:04 -0800

    DataSet actions take ids directly.

--- tester/tester.go
+++ tester/tester.go
@@ -17,9 +17,8 @@ type options struct {
 	ConfigureAp bool
 	Schedules   bool
 
-	DownloadDataSet bool
-	EraseDataSet    bool
-	DataSetId       int
+	DownloadDataSet int
+	EraseDataSet    int
 
 	LiveDataPoll     bool
 	LiveDataInterval int
@@ -38,15 +37,16 @@ func main() {
 	flag.StringVar(&o.Address, "address", "", "ip address of the device")
 	flag.IntVar(&o.Port, "port", 12345, "port number")
 	flag.BoolVar(&o.Scan, "scan", false, "scan the device's capabilities and data sets")
-	flag.BoolVar(&o.Network, "network", false, "query the device's network settings")
-	flag.BoolVar(&o.ConfigureAp, "network-configure-ap", false, "force network ap mode (empty configured networks)")
-	flag.BoolVar(&o.Files, "files", false, "scan the device's files")
-	flag.IntVar(&o.DownloadFile, "download-file", -1, "download file")
 	flag.BoolVar(&o.Schedules, "schedules", false, "query for schedules")
 
-	flag.BoolVar(&o.DownloadDataSet, "download-ds", false, "download data")
-	flag.BoolVar(&o.EraseDataSet, "erase-ds", false, "erase data")
-	flag.IntVar(&o.DataSetId, "data-set", 0, "data set id to download or erase")
+	flag.BoolVar(&o.Network, "network", false, "query the device's network settings")
+	flag.BoolVar(&o.ConfigureAp, "network-configure-ap", false, "force network ap mode (empty configured networks)")
+
+	flag.BoolVar(&o.Files, "files", false, "scan the device's files")
+	flag.IntVar(&o.DownloadFile, "download-file", -1, "download file")
+
+	flag.IntVar(&o.DownloadDataSet, "download-ds", -1, "download data")
+	flag.IntVar(&o.EraseDataSet, "erase-ds", -1, "erase data")
 
 	flag.IntVar(&o.LiveDataInterval, "live-data-interval", 1000, "interval to poll (0 disables)")
 	flag.BoolVar(&o.LiveDataPoll, "live-data-poll", false, "send live data poll")
@@ -124,22 +124,22 @@ func main() {
 		}
 	}
 
-	if o.DownloadDataSet {
-		ds, err := device.QueryDataSet(uint32(o.DataSetId))
+	if o.DownloadDataSet >= 0 {
+		ds, err := device.QueryDataSet(uint32(o.DownloadDataSet))
 		if err != nil {
 			log.Fatalf("Error: %v", err)
 		}
 
-		device.DownloadDataSet(uint32(o.DataSetId), ds.DataSets.DataSets[0].Pages)
+		device.DownloadDataSet(uint32(o.DownloadDataSet), ds.DataSets.DataSets[0].Pages)
 	}
 
-	if o.EraseDataSet {
-		_, err := device.QueryDataSet(uint32(o.DataSetId))
+	if o.EraseDataSet >= 0 {
+		_, err := device.QueryDataSet(uint32(o.EraseDataSet))
 		if err != nil {
 			log.Fatalf("Error: %v", err)
 		}
 
-		_, err = device.EraseDataSet(uint32(o.DataSetId))
+		_, err = device.EraseDataSet(uint32(o.EraseDataSet))
 		if err != nil {
 			log.Fatalf("Error: %v", err)
 		}
