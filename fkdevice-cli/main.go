d506165d324814c8832c291119f094f0fb24d9be
Jacob Lewallen <jlewallen@gmail.com>
2019-08-24 18:19:43 -0700

    Adding get/take readings.

--- fkdevice-cli/main.go
+++ fkdevice-cli/main.go
@@ -1,66 +1,20 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
-	pb "github.com/fieldkit/app-protocol"
-	fkc "github.com/fieldkit/app-protocol/fkdevice"
-	fkatlaspb "github.com/fieldkit/atlas-protocol"
-	testing "github.com/fieldkit/cloud/server/tools"
-	"github.com/golang/protobuf/proto"
-	progress "gopkg.in/cheggaaa/pb.v1"
-	"io"
 	"log"
 	"os"
-	"time"
+
+	fkc "github.com/fieldkit/app-protocol/fkdevice"
 )
 
 type options struct {
-	Address  string
-	Port     int
-	HttpMode bool
-
-	Scan        bool
-	Status      bool
-	Readings    bool
-	Network     bool
-	ConfigureAp bool
-	Schedules   bool
-
-	LiveDataOnce     bool
-	LiveDataPoll     bool
-	LiveDataInterval int
-
-	Files           bool
-	DownloadFile    int
-	EraseFile       int
-	WriteTo         string
-	MetadataOnly    bool
-	PrependMetadata bool
-
-	Offset int
-	Length int
-
-	Metadata bool
-
-	Identity bool
-	Device   string
-	Stream   string
-
-	Reset  bool
-	Format bool
-
-	DeviceName   string
-	LinkIdentity bool
-
-	AtlasQuery int
-
-	Host     string
-	Scheme   string
-	Username string
-	Password string
-	Project  string
+	Address      string
+	Port         int
+	Status       bool
+	Name         string
+	GetReadings  bool
+	TakeReadings bool
 }
 
 func main() {
@@ -68,50 +22,10 @@ func main() {
 
 	flag.StringVar(&o.Address, "address", "", "ip address of the device")
 	flag.IntVar(&o.Port, "port", 80, "port number")
-	flag.BoolVar(&o.HttpMode, "http", true, "use http instead of tcp")
-
-	flag.BoolVar(&o.Scan, "scan", false, "scan the device's capabilities and data sets")
-	flag.BoolVar(&o.Schedules, "schedules", false, "query for schedules")
 	flag.BoolVar(&o.Status, "status", false, "device status")
-	flag.BoolVar(&o.Readings, "readings", false, "readings")
-
-	flag.BoolVar(&o.Network, "network", false, "query the device's network settings")
-	flag.BoolVar(&o.ConfigureAp, "network-configure-ap", false, "force network ap mode (empty configured networks)")
-
-	flag.StringVar(&o.WriteTo, "write", "", "file to write to")
-
-	flag.BoolVar(&o.Files, "files", false, "scan the device's files")
-	flag.IntVar(&o.DownloadFile, "download-file", -1, "download file")
-	flag.IntVar(&o.EraseFile, "erase-file", -1, "erase file")
-	flag.BoolVar(&o.MetadataOnly, "metadata-only", false, "only get metadata")
-	flag.BoolVar(&o.PrependMetadata, "prepend-metadata", false, "include metadata")
-
-	flag.IntVar(&o.Offset, "offset", 0, "download offset")
-	flag.IntVar(&o.Length, "length", 0, "download length")
-
-	flag.BoolVar(&o.Metadata, "metadata", false, "query metadata")
-
-	flag.IntVar(&o.LiveDataInterval, "live-data-interval", 1000, "interval to poll (0 disables)")
-	flag.BoolVar(&o.LiveDataPoll, "live-data-poll", false, "send live data poll")
-	flag.BoolVar(&o.LiveDataOnce, "live-data-once", false, "one live data query")
-
-	flag.BoolVar(&o.Identity, "identity", false, "retrieve the device's identity")
-	flag.StringVar(&o.Device, "device", "", "device identity")
-	flag.StringVar(&o.Stream, "stream", "", "stream identity")
-
-	flag.BoolVar(&o.Reset, "reset", false, "reset the device")
-	flag.BoolVar(&o.Format, "format", false, "format the device")
-
-	flag.StringVar(&o.DeviceName, "device-name", "test-device", "device name")
-	flag.BoolVar(&o.LinkIdentity, "link", false, "link device identity to website")
-
-	flag.StringVar(&o.Host, "host", "127.0.0.1:8080", "hostname to use")
-	flag.StringVar(&o.Scheme, "scheme", "http", "scheme to use")
-	flag.StringVar(&o.Username, "username", "demo-user", "username to use")
-	flag.StringVar(&o.Password, "password", "asdfasdfasdf", "password to use")
-	flag.StringVar(&o.Project, "project", "www", "project")
-
-	flag.IntVar(&o.AtlasQuery, "atlas-query", -1, "test atlas module queries")
+	flag.StringVar(&o.Name, "name", "", "name")
+	flag.BoolVar(&o.GetReadings, "get", false, "device status")
+	flag.BoolVar(&o.TakeReadings, "take", false, "device status")
 
 	flag.Parse()
 
@@ -124,14 +38,6 @@ func main() {
 		Address:   o.Address,
 		Port:      o.Port,
 		Callbacks: &fkc.LogJsonCallbacks{},
-		HttpMode:  o.HttpMode,
-	}
-
-	if o.Scan {
-		_, err := device.QueryCapabilities()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
 	}
 
 	if o.Status {
@@ -141,202 +47,22 @@ func main() {
 		}
 	}
 
-	if o.Readings {
+	if o.Name != "" {
+		_, err := device.ConfigureName(o.Name)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+	}
+
+	if o.GetReadings {
+		_, err := device.QueryGetReadings()
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+	}
+
+	if o.TakeReadings {
 		_, err := device.QueryTakeReadings()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.Metadata {
-		_, err := device.QueryMetadata()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.Schedules {
-		_, err := device.QuerySchedules()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.Network {
-		_, err := device.QueryNetworkSettings()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.ConfigureAp {
-		_, err := device.ConfigureNetworkSettings(&pb.NetworkSettings{
-			CreateAccessPoint: int32(1),
-			Networks:          []*pb.NetworkInfo{},
-		})
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.LiveDataPoll {
-		for {
-			_, err := device.QueryLiveData(o.LiveDataInterval)
-			if err != nil {
-				log.Fatalf("Error: %v", err)
-			}
-
-			if o.LiveDataInterval > 0 {
-				time.Sleep(time.Duration(o.LiveDataInterval) * time.Millisecond)
-			} else {
-				break
-			}
-		}
-	}
-
-	if o.LiveDataOnce {
-		_, err := device.QueryLiveData(o.LiveDataInterval)
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.Files {
-		_, err := device.QueryFiles()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.DownloadFile >= 0 {
-		file, err := device.QueryFileInformation(uint32(o.DownloadFile))
-		if file.Size == 0 {
-			log.Fatalf("File is empty, ignoring")
-		}
-
-		fileName := o.WriteTo
-		if fileName == "" {
-			fileName = fmt.Sprintf("%s_%d", file.Name, file.Version)
-		}
-		log.Printf("Opening %s", fileName)
-
-		f, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
-		if err != nil {
-			log.Fatalf("Unable to open %s (%v)", fileName, err)
-		}
-
-		defer f.Close()
-
-		size := int(file.Size)
-		if o.Length > 0 {
-			size = o.Length
-		}
-		bar := progress.New(size).SetUnits(progress.U_BYTES)
-		bar.Start()
-
-		writer := io.MultiWriter(f, bar)
-
-		flags := 0
-		if o.MetadataOnly {
-			flags |= int(pb.DownloadFlags_DOWNLOAD_FLAG_METADATA_ONLY)
-		}
-		if o.PrependMetadata {
-			flags |= int(pb.DownloadFlags_DOWNLOAD_FLAG_METADATA_PREPEND)
-		}
-		err = device.DownloadFileToWriter(uint32(o.DownloadFile), uint32(o.Offset), uint32(o.Length), uint32(flags), writer)
-		if err != nil {
-			log.Fatalf("Unable to download file %s (%v)", fileName, err)
-		}
-
-		// NOTE: This is wrong if they only ask for metadata.
-		bar.Set(int(file.Size))
-		bar.Finish()
-	}
-
-	if o.EraseFile >= 0 {
-		_, err := device.EraseFile(uint32(o.EraseFile))
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.Device != "" && o.Stream != "" {
-		_, err := device.ConfigureIdentity(o.Device, o.Stream)
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.Identity {
-		_, err := device.QueryIdentity()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.AtlasQuery >= 0 {
-		moduleQuery := &fkatlaspb.WireAtlasQuery{
-			Type: fkatlaspb.QueryType_QUERY_ATLAS_COMMAND,
-			AtlasCommand: &fkatlaspb.AtlasCommand{
-				Sensor:  fkatlaspb.SensorType_TEMP,
-				Command: "I",
-			},
-		}
-		data, err := proto.Marshal(moduleQuery)
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-
-		reply, err := device.QueryModule(8, data)
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-
-		if reply.Module != nil {
-			moduleReply := &fkatlaspb.WireAtlasReply{}
-
-			err = proto.Unmarshal(reply.Module.Message, moduleReply)
-			if err != nil {
-				log.Fatalf("Error: %v", err)
-			}
-
-			log.Printf("%v", moduleReply)
-		} else {
-			log.Printf("%v", reply)
-		}
-	}
-
-	if o.LinkIdentity {
-		ctx := context.TODO()
-		c, err := testing.CreateAndAuthenticate(ctx, o.Host, o.Scheme, o.Username, o.Password)
-		if err != nil {
-			log.Fatalf("%v", err)
-		}
-
-		webDevice, err := testing.CreateWebDevice(ctx, c, o.Project, o.DeviceName, "", "1")
-		if err != nil {
-			log.Fatalf("%v", err)
-		}
-
-		log.Printf("%v\n", webDevice)
-
-		newIdentity, err := device.ConfigureIdentity(webDevice.Key, "1")
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-
-		log.Printf("%v\n", newIdentity)
-	}
-
-	if o.Reset {
-		_, err := device.Reset()
-		if err != nil {
-			log.Fatalf("Error: %v", err)
-		}
-	}
-
-	if o.Format {
-		_, err := device.Format()
 		if err != nil {
 			log.Fatalf("Error: %v", err)
 		}
--- fkdevice/fkdevice.go
+++ fkdevice/fkdevice.go
@@ -23,37 +23,21 @@ const (
 )
 
 type DeviceClientLoggingCallbacks interface {
-	Sent(query *pb.WireMessageQuery)
-	Received(reply *pb.WireMessageReply)
-	HttpSent(query *pb.HttpQuery)
-	HttpReceived(reply *pb.HttpReply)
+	Sent(query *pb.HttpQuery)
+	Received(reply *pb.HttpReply)
 }
 
 type LogJsonCallbacks struct {
 }
 
-func (cb *LogJsonCallbacks) Sent(query *pb.WireMessageQuery) {
+func (cb *LogJsonCallbacks) Sent(query *pb.HttpQuery) {
 	queryJson, err := json.MarshalIndent(query, "", "  ")
 	if err == nil {
 		log.Printf("Sending: %s", queryJson)
 	}
 }
 
-func (cb *LogJsonCallbacks) Received(reply *pb.WireMessageReply) {
-	replyJson, err := json.MarshalIndent(reply, "", "  ")
-	if err == nil {
-		log.Printf("Received: %s", replyJson)
-	}
-}
-
-func (cb *LogJsonCallbacks) HttpSent(query *pb.HttpQuery) {
-	queryJson, err := json.MarshalIndent(query, "", "  ")
-	if err == nil {
-		log.Printf("Sending: %s", queryJson)
-	}
-}
-
-func (cb *LogJsonCallbacks) HttpReceived(reply *pb.HttpReply) {
+func (cb *LogJsonCallbacks) Received(reply *pb.HttpReply) {
 	replyJson, err := json.MarshalIndent(reply, "", "  ")
 	if err == nil {
 		log.Printf("Received: %s", replyJson)
@@ -64,18 +48,9 @@ type DeviceClient struct {
 	Callbacks DeviceClientLoggingCallbacks
 	Address   string
 	Port      int
-	HttpMode  bool
 }
 
-func (d *DeviceClient) QueryCapabilities() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_CAPABILITIES)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) QueryStatus() (*pb.WireMessageReply, error) {
+func (d *DeviceClient) QueryStatus() (*pb.HttpReply, error) {
 	reply, err := d.queryDevice(pb.QueryType_QUERY_STATUS)
 	if err != nil {
 		return nil, err
@@ -83,112 +58,11 @@ func (d *DeviceClient) QueryStatus() (*pb.WireMessageReply, error) {
 	return reply, nil
 }
 
-func (d *DeviceClient) QueryTakeReadings() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_TAKE_READINGS)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) QuerySchedules() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_SCHEDULES)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) Reset() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_RESET)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) Format() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_FORMAT)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) QueryFiles() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_FILES)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) EraseFile(id uint32) (*pb.WireMessageReply, error) {
-	query := &pb.WireMessageQuery{
-		Type: pb.QueryType_QUERY_ERASE_FILE,
-		EraseFile: &pb.EraseFile{
-			Id: uint32(id),
-		},
-	}
-	reply, err := d.queryDeviceQuery(query)
-	if err != nil {
-		return nil, err
-	}
-
-	return reply, nil
-}
-
-func (d *DeviceClient) QueryLiveData(liveDataInterval int) (*pb.WireMessageReply, error) {
-	query := &pb.WireMessageQuery{
-		Type: pb.QueryType_QUERY_LIVE_DATA_POLL,
-		LiveDataPoll: &pb.LiveDataPoll{
-			Interval: uint32(liveDataInterval),
-		},
-	}
-	reply, err := d.queryDeviceQuery(query)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) QueryNetworkSettings() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_NETWORK_SETTINGS)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) ConfigureNetworkSettings(ns *pb.NetworkSettings) (*pb.WireMessageReply, error) {
-	query := &pb.WireMessageQuery{
-		Type: pb.QueryType_QUERY_CONFIGURE_NETWORK_SETTINGS,
-		NetworkSettings: &pb.NetworkSettings{
-			CreateAccessPoint: ns.CreateAccessPoint,
-			Networks:          ns.Networks,
-		},
-	}
-	reply, err := d.queryDeviceQuery(query)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) QueryIdentity() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_IDENTITY)
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
-}
-
-func (d *DeviceClient) ConfigureIdentity(device, stream string) (*pb.WireMessageReply, error) {
-	query := &pb.WireMessageQuery{
-		Type: pb.QueryType_QUERY_CONFIGURE_IDENTITY,
+func (d *DeviceClient) ConfigureName(name string) (*pb.HttpReply, error) {
+	query := &pb.HttpQuery{
+		Type: pb.QueryType_QUERY_CONFIGURE,
 		Identity: &pb.Identity{
-			Device: device,
-			Stream: stream,
+			Name: name,
 		},
 	}
 	reply, err := d.queryDeviceQuery(query)
@@ -198,37 +72,16 @@ func (d *DeviceClient) ConfigureIdentity(device, stream string) (*pb.WireMessage
 	return reply, nil
 }
 
-func (d *DeviceClient) QueryFileInformation(id uint32) (*pb.File, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_FILES)
-	if err != nil {
-		return nil, err
-	}
-	for _, file := range reply.Files.Files { // Files, files, files!
-		if file.Id == id {
-			return file, nil
-		}
-	}
-	return nil, nil
-}
-
-func (d *DeviceClient) QueryModule(id uint32, message []byte) (*pb.WireMessageReply, error) {
-	query := &pb.WireMessageQuery{
-		Type: pb.QueryType_QUERY_MODULE,
-		Module: &pb.QueryModule{
-			Id:      id,
-			Address: id,
-			Message: message,
-		},
-	}
-	reply, err := d.queryDeviceQuery(query)
+func (d *DeviceClient) QueryGetReadings() (*pb.HttpReply, error) {
+	reply, err := d.queryDevice(pb.QueryType_QUERY_GET_READINGS)
 	if err != nil {
 		return nil, err
 	}
 	return reply, nil
 }
 
-func (d *DeviceClient) QueryMetadata() (*pb.WireMessageReply, error) {
-	reply, err := d.queryDevice(pb.QueryType_QUERY_METADATA)
+func (d *DeviceClient) QueryTakeReadings() (*pb.HttpReply, error) {
+	reply, err := d.queryDevice(pb.QueryType_QUERY_TAKE_READINGS)
 	if err != nil {
 		return nil, err
 	}
@@ -247,43 +100,6 @@ func (dr *LimitedReader) Read(p []byte) (n int, err error) {
 		return 0, io.EOF
 	}
 	return dr.Target.Read(p)
-}
-
-func (d *DeviceClient) DownloadFileToWriter(id, offset, length, flags uint32, f io.Writer) error {
-	query := &pb.WireMessageQuery{
-		Type: pb.QueryType_QUERY_DOWNLOAD_FILE,
-		DownloadFile: &pb.DownloadFile{
-			Id:     id,
-			Offset: offset,
-			Length: length,
-			Flags:  flags,
-		},
-	}
-
-	headerCallback := CallbackFunc(func(reply *pb.WireMessageReply) error {
-		if reply.Type != pb.ReplyType_REPLY_BUSY {
-			if reply.FileData != nil {
-				_, err := f.Write(reply.FileData.Data)
-				if err != nil {
-					return fmt.Errorf("Unable to write to file: %v", err)
-				}
-			}
-		} else {
-			return DeviceBusyErr
-		}
-		return nil
-	})
-
-	bodyCallback := BodyCallbackFunc(func(r io.Reader) error {
-		_, err := io.Copy(f, r)
-		return err
-	})
-
-	opts := DeviceQueryOpts{nil, query, 10}
-
-	_, err := d.queryDeviceDownload(&opts, headerCallback, bodyCallback)
-
-	return err
 }
 
 type CallbackFunc func(*pb.WireMessageReply) error
@@ -368,11 +184,7 @@ func (d *DeviceClient) openAndSendQueryHttp(query *pb.HttpQuery) (*QueryResponse
 }
 
 func (d *DeviceClient) openAndSendQuery(opts *DeviceQueryOpts) (*QueryResponse, error) {
-	if d.HttpMode {
-		return d.openAndSendQueryHttp(opts.httpQuery)
-	} else {
-		return d.openAndSendQueryTcp(opts.wireMessageQuery)
-	}
+	return d.openAndSendQueryHttp(opts.httpQuery)
 }
 
 type DeadlineReader struct {
@@ -396,9 +208,8 @@ func (dr *DebugReader) Read(p []byte) (n int, err error) {
 }
 
 type DeviceQueryOpts struct {
-	httpQuery        *pb.HttpQuery
-	wireMessageQuery *pb.WireMessageQuery
-	timeout          int
+	httpQuery *pb.HttpQuery
+	timeout   int
 }
 
 func (d *DeviceClient) queryDeviceCallback(opts *DeviceQueryOpts, callback CallbackFunc) (collection.MessageCollection, error) {
@@ -412,28 +223,13 @@ func (d *DeviceClient) queryDeviceCallback(opts *DeviceQueryOpts, callback Callb
 	dr := c.Reader(opts.timeout)
 
 	unmarshalFunc := message.UnmarshalFunc(func(b []byte) (proto.Message, error) {
-		if d.HttpMode {
-			var reply pb.HttpReply
-			err := proto.Unmarshal(b, &reply)
-			if err != nil {
-				return nil, err
-			}
-
-			return &reply, nil
-		} else {
-			var reply pb.WireMessageReply
-			err := proto.Unmarshal(b, &reply)
-			if err != nil {
-				return nil, err
-			}
-
-			err = callback(&reply)
-			if err != nil {
-				return nil, err
-			}
-
-			return &reply, err
+		var reply pb.HttpReply
+		err := proto.Unmarshal(b, &reply)
+		if err != nil {
+			return nil, err
 		}
+
+		return &reply, nil
 	})
 
 	collection, err := stream.ReadLengthPrefixedCollection(dr, unmarshalFunc)
@@ -448,109 +244,35 @@ func (d *DeviceClient) queryDeviceCallback(opts *DeviceQueryOpts, callback Callb
 	return collection, err
 }
 
-func (d *DeviceClient) queryDeviceDownload(opts *DeviceQueryOpts, callback CallbackFunc, body BodyCallbackFunc) ([]*pb.WireMessageReply, error) {
-	c, err := d.openAndSendQuery(opts)
-	if err != nil {
-		return nil, err
-	}
-
-	defer c.Close()
-
-	dr := c.Reader(120)
-	lr := &LimitedReader{
-		Target:           dr,
-		MessagesExpected: 1,
-	}
-
-	unmarshalFunc := message.UnmarshalFunc(func(b []byte) (proto.Message, error) {
-		var reply pb.WireMessageReply
-		err := proto.Unmarshal(b, &reply)
-		if err != nil {
-			return nil, err
-		}
-
-		err = callback(&reply)
-		if err != nil {
-			return nil, err
-		}
-
-		lr.MessagesExpected -= 1
-		return &reply, err
-	})
-
-	collection, err := stream.ReadLengthPrefixedCollection(lr, unmarshalFunc)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(collection) == 0 {
-		return nil, fmt.Errorf("No reply.")
-	}
-
-	err = body(dr)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading body: %v", err)
-	}
-
-	replies := make([]*pb.WireMessageReply, 0)
-
-	for _, m := range collection {
-		replies = append(replies, m.(*pb.WireMessageReply))
-	}
-
-	return replies, err
-}
-
-func (d *DeviceClient) queryDeviceOpts(opts *DeviceQueryOpts) (reply *pb.WireMessageReply, err error) {
+func (d *DeviceClient) queryDeviceOpts(opts *DeviceQueryOpts) (reply *pb.HttpReply, err error) {
 	if d.Callbacks != nil {
-		if opts.wireMessageQuery != nil {
-			d.Callbacks.Sent(opts.wireMessageQuery)
-		} else {
-			d.Callbacks.HttpSent(opts.httpQuery)
-		}
+		d.Callbacks.Sent(opts.httpQuery)
 	}
 	replies, err := d.queryDeviceMultiple(opts)
 	if err != nil {
 		return nil, err
 	}
 	if len(replies) > 0 {
-		if d.HttpMode {
-			if d.Callbacks != nil {
-				for _, m := range replies {
-					replies = append(replies, m.(*pb.HttpReply))
-					d.Callbacks.HttpReceived(m.(*pb.HttpReply))
-				}
+		if d.Callbacks != nil {
+			for _, m := range replies {
+				replies = append(replies, m.(*pb.HttpReply))
+				d.Callbacks.Received(m.(*pb.HttpReply))
 			}
-			return nil, nil
-		} else {
-			if d.Callbacks != nil {
-				for _, m := range replies {
-					replies = append(replies, m.(*pb.WireMessageReply))
-					d.Callbacks.Received(m.(*pb.WireMessageReply))
-				}
-			}
-			return replies[0].(*pb.WireMessageReply), nil
 		}
+		return nil, nil
 	}
 	return nil, nil
 }
 
-func (d *DeviceClient) queryDevice(queryType pb.QueryType) (reply *pb.WireMessageReply, err error) {
-	if d.HttpMode {
-		query := &pb.HttpQuery{
-			Type: queryType,
-		}
-		return d.queryDeviceOpts(&DeviceQueryOpts{query, nil, DefaultTimeout})
-	} else {
-		query := &pb.WireMessageQuery{
-			Type: queryType,
-		}
-		return d.queryDeviceOpts(&DeviceQueryOpts{nil, query, DefaultTimeout})
+func (d *DeviceClient) queryDevice(queryType pb.QueryType) (reply *pb.HttpReply, err error) {
+	query := &pb.HttpQuery{
+		Type: queryType,
 	}
+	return d.queryDeviceOpts(&DeviceQueryOpts{query, DefaultTimeout})
 }
 
-func (d *DeviceClient) queryDeviceQuery(query *pb.WireMessageQuery) (reply *pb.WireMessageReply, err error) {
-	return d.queryDeviceOpts(&DeviceQueryOpts{nil, query, DefaultTimeout})
+func (d *DeviceClient) queryDeviceQuery(query *pb.HttpQuery) (reply *pb.HttpReply, err error) {
+	return d.queryDeviceOpts(&DeviceQueryOpts{query, DefaultTimeout})
 }
 
 func (d *DeviceClient) queryDeviceMultiple(opts *DeviceQueryOpts) (replies collection.MessageCollection, err error) {
